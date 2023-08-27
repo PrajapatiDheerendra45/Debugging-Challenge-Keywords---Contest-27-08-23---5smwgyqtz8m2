357888e97552a733825c6f14342b88a0a3a7e9db
PrajapatiDheerendra45 <135729261+PrajapatiDheerendra45@users.noreply.github.com>
2023-08-27 11:50:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,11 @@
-import React, { useEffect, useState } from "react";
-import '../styles/App.css';
-
-const App = () => {
-
-  let [count, setCount] = useState(0)
-
+import './index.css'; // Link to your CSS file
+import React,{useEffect,useState} from "react";
+export function App() {
+  let[count,setCount]=useState(0)
   return (
-    <div class="ball">
-      <h1 class="count" ondoubleclick={() => { alert("cant edit it") }}>{count}</h1>
-      <button class='increment-button' onclick={() => { setCount(count + 1) }}>Increment</button>
+    <div className="ball">
+    <h1 className="count" onDoubleClick={()=>{alert("cant edit it")}}>{count}</h1>
+    <button className='increment-button' onClick={()=>{setCount(count+1)}}>Increment</button>
     </div>
   )
 }
-
-
-export default App;
